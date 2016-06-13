791811a0d89ff2fb64f35588c42cd5eae01e19e3
iceberg20 <italoberg@yahoo.com.br>
2016-06-12 22:00:56 -0300

    Chapter 10.2 finished

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -10,6 +10,17 @@ class CartsController < ApplicationController
   # GET /carts/1
   # GET /carts/1.json
   def show
+    begin
+      @cart = Cart.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+      logger.error "Attempt to access invalid cart #{params[:id]}"
+      redirect_to store_url, notice: 'Invalid cart'
+      else
+        respond_to do |format|
+        format.html # show.html.erb
+        format.json { render json: @cart }
+      end
+    end
   end
 
   # GET /carts/new
@@ -65,6 +76,9 @@ class CartsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_cart
       @cart = Cart.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+      logger.error "Attempt to access invalid cart #{params[:id]}"
+      redirect_to store_url, notice: 'Invalid cart'
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
